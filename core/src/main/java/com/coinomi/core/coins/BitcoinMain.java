243c8a6040c472cd7e114c25383cf4b23adc6cea
Giannis Dzegoutanis <erasmospunk@gmail.com>
2015-01-13 21:49:07 +0200

    revert btc fees to previous values as they cause delays

--- core/src/main/java/com/coinomi/core/coins/BitcoinMain.java
+++ core/src/main/java/com/coinomi/core/coins/BitcoinMain.java
@@ -20,8 +20,8 @@ public class BitcoinMain extends CoinType {
         uriScheme = "bitcoin";
         bip44Index = 0;
         unitExponent = 8;
-        feePerKb = Coin.valueOf(1000);
-        minNonDust = Coin.valueOf(546);
+        feePerKb = Coin.valueOf(10000);
+        minNonDust = Coin.valueOf(5460);
         softDustLimit = Coin.valueOf(1000000); // 0.01 BTC
         softDustPolicy = SoftDustPolicy.AT_LEAST_BASE_FEE_IF_SOFT_DUST_TXO_PRESENT;
     }
