9a20a0cd5b9f0e94892d778fd675a0515c961f0f
John L. Jegutanis <erasmospunk@gmail.com>
2015-08-13 23:47:47 +0200

    added missing message interfaces

---
+++ core/src/main/java/com/coinomi/core/messages/MessageFactory.java
@@ -0,0 +1,24 @@
+package com.coinomi.core.messages;
+
+import org.bitcoinj.core.Transaction;
+
+import java.io.Serializable;
+
+import javax.annotation.Nullable;
+
+/**
+ * @author John L. Jegutanis
+ */
+public interface MessageFactory {
+    int maxMessageSize();
+
+    boolean canHandlePublicMessages();
+
+    boolean canHandlePrivateMessages();
+
+    TxMessage createPublicMessage(String message);
+
+    // TODO change to abstract transaction
+    @Nullable
+    TxMessage extractPublicMessage(Transaction transaction);
+}
