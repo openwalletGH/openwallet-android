a13e8d97ab2f2333cd9c9d5f20022e4f6864bc35
John L. Jegutanis <erasmospunk@gmail.com>
2015-10-02 15:50:59 +0300

    update Nubits fees

--- core/src/main/java/com/coinomi/core/coins/NuBitsMain.java
+++ core/src/main/java/com/coinomi/core/coins/NuBitsMain.java
@@ -20,7 +20,7 @@ public class NuBitsMain extends CoinType {
         uriScheme = "nu";
         bip44Index = 12;
         unitExponent = 4;
-        feePerKb = value(100); // 0.01NBT, careful NuBits has 10000 units per coin
+        feePerKb = value(200); // 0.02NBT, careful NuBits has 10000 units per coin
         minNonDust = value(100); // 0.01NBT
         softDustLimit = minNonDust;
         softDustPolicy = SoftDustPolicy.NO_POLICY;
