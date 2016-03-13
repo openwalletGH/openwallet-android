6e8c5f7e9b7e1792a3aa147713a0d33a15d9fabc
John L. Jegutanis <erasmospunk@gmail.com>
2016-03-13 20:31:43 +0100

    changed a font-icon in the show recovery phrase screen

--- wallet/src/main/java/com/coinomi/wallet/Constants.java
+++ wallet/src/main/java/com/coinomi/wallet/Constants.java
@@ -92,7 +92,7 @@ public class Constants {
     public static final int HTTP_TIMEOUT_MS = 15 * (int) DateUtils.SECOND_IN_MILLIS;
 
     public static final String TX_CACHE_NAME = "tx_cache";
-    public static final int TX_CACHE_SIZE = 5 * 1024 * 1024; // 5 MiB
+    public static final int TX_CACHE_SIZE = 5 * 1024 * 1024; // 5 MiB, TODO currently not enforced
 
     public static final long RATE_UPDATE_FREQ_MS = 30 * DateUtils.SECOND_IN_MILLIS;
 
